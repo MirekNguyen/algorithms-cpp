415306c5ecedd759a7167274711cd96feb67d104
Mirek Nguyen <nguyemir@fit.cvut.cz>
2022-12-22 21:04:02 +0100

    fix(dijkstra): solve function

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -146,14 +146,21 @@ struct Dijkstra {
   vector<string> solve(string start, string end) {
     solve(start);
     vector<string> v;
+    bool found;
     while (true) {
+      found = false;
       for (int i = 0; i < size; i++) {
         if (array[i]->start == end) { // find end
           v.push_back(end);
           end = array[i]->end;
+          found = true;
         }
       }
-      if (end == start || end == "") {
+      if (end == start) {
+        v.push_back(start);
+        return v;
+      }
+      if (end == "" || !found) {
         return v;
       }
     }
@@ -167,26 +174,48 @@ void printReverse(vector<string> v) {
 }
 
 int main() {
-  Graph g(true);
-  g.insert("s", "a", 6);
-  g.insert("s", "b", 2);
-  g.insert("s", "c", 3);
-  g.insert("s", "d", 9);
-  g.insert("a", "b", 3);
-  g.insert("a", "e", 4);
-  g.insert("b", "e", 7);
-  g.insert("b", "f", 3);
-  g.insert("b", "c", 2);
-  g.insert("c", "d", 5);
-  g.insert("c", "f", 1);
-  g.insert("d", "g", 1);
-  g.insert("e", "f", 3);
-  g.insert("f", "g", 2);
+  Graph g(false);
+  // g.insert("s", "a", 6);
+  // g.insert("s", "b", 2);
+  // g.insert("s", "c", 3);
+  // g.insert("s", "d", 9);
+  // g.insert("a", "b", 3);
+  // g.insert("a", "e", 4);
+  // g.insert("b", "e", 7);
+  // g.insert("b", "f", 3);
+  // g.insert("b", "c", 2);
+  // g.insert("c", "d", 5);
+  // g.insert("c", "f", 1);
+  // g.insert("d", "g", 1);
+  // g.insert("e", "f", 3);
+  // g.insert("f", "g", 2);
+  g.insert("A", "B", 2);
+  g.insert("A", "E", 7);
+  g.insert("A", "F", 4);
+  g.insert("B", "F", 3);
+  g.insert("B", "G", 6);
+  g.insert("B", "C", 2);
+  g.insert("C", "G", 4);
+  g.insert("C", "D", 2);
+  g.insert("D", "G", 1);
+  g.insert("D", "H", 10);
+  g.insert("E", "I", 1);
+  g.insert("F", "E", 1);
+  g.insert("F", "I", 1);
+  g.insert("F", "G", 2);
+  g.insert("G", "J", 2);
+  g.insert("G", "K", 4);
+  g.insert("H", "G", 2);
+  g.insert("H", "L", 1);
+  g.insert("J", "F", 1);
+  g.insert("J", "I", 2);
+  g.insert("J", "K", 1);
+  g.insert("K", "H", 1);
 
   // g.print();
   Dijkstra d;
   d.get_vertices(g);
-  printReverse(d.solve("s", "e"));
+  printReverse(d.solve("A", "L"));
   cout << endl;
   d.print();
   return 0;
